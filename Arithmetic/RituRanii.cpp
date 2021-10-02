b9dc68ee82fc16fe2e090dc8c255236d18d504b8
mbul48 <45551081+mbul48@users.noreply.github.com>
2021-10-03 03:01:17 +0700

    Update RituRanii.cpp

--- Arithmetic/RituRanii.cpp
+++ Arithmetic/RituRanii.cpp
@@ -5,7 +5,7 @@ int main()
     int a,b,c;
     cout << "Enter two integers for addition\n";
     cin >> a >> b;
-    c = a + b;
+    c = a + b + 50; //extra 50
     cout << "sum of the numbers: " << c <<endl;
     return 0;
 }
