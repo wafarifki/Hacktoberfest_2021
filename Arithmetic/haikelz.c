f1d8db2b25d93b97b60ab66ad9a2329959ea1d4d
Haikel <77146709+haikelz@users.noreply.github.com>
2021-10-02 16:22:07 +0700

    Add haikelz.c

---
+++ Arithmetic/haikelz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+    
+    int alas, tinggi, hasil; 
+    alas = 10;
+    tinggi = 17; 
+    hasil = (alas * tinggi) / 2;
+    
+    // Hasil
+    printf("Diketahui sebuah segitiga dengan alas 10 cm, dan tinggi 17 cm, maka luasnya adalah: %d cm^2\n",hasil );
+}
