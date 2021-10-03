907860427f0fa9de1311bf55eb70e0b0a6b131f7
Kevin Rivaldo Panggabean <82790760+krpauto@users.noreply.github.com>
2021-10-03 18:20:16 +0700

    create

---
+++ Arithmetic/operatormodkrpauto.c
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  int a, b, c, d;
+ 
+  a = 8 % 4;
+  b = 8 % 5;
+  c = 10 % 2;
+  d = 100 % 7;
+ 
+  cout << "Isi variabel a: " << a << endl;
+  cout << "Isi variabel b: " << b << endl;
+  cout << "Isi variabel c: " << c << endl;
+  cout << "Isi variabel d: " << d << endl;
+ 
+  return 0;
+}
