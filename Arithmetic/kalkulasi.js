721efdd7823492c87660e059980472c81c14e972
syamsiimanpermana <72720178+syamsiimanpermana@users.noreply.github.com>
2021-10-03 08:34:15 +0700

    menambahkan file kalkulasi.js

---
+++ Arithmetic/kalkulasi.js
@@ -0,0 +1,15 @@
+function kalkulasi(input) {
+	return eval(input);
+}
+
+// contoh operasi perkalian
+console.log(kalkulasi(12*3));
+
+// contoh operasi pembagian
+console.log(kalkulasi(12/3));
+
+// contoh operasi pengurangan
+console.log(kalkulasi(12-3));
+
+// contoh operasi campuran
+console.log(kalkulasi(23+1223*12-12));
