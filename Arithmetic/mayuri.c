43cfaef162615f936b0c04eb81b459d227c898b6
Mayuri shinde <85113641+Mayurishinde27@users.noreply.github.com>
2021-10-03 18:12:39 +0530

    Update mayuri.c

--- Arithmetic/mayuri.c
+++ Arithmetic/mayuri.c
@@ -1,13 +1,33 @@
 #include<stdio.h>
-int  main( ) 
+int main()
 {
 	int i;
 	int j;
-	int sum=0;
-	printf("Enter the numbers\n");
-	scanf("%d %d",&i,&j);
-	sum = i+j;
-	printf("%d",sum);
-	return 0;
+	int c;
+	printf("Enter the number\n");
+	scanf("%d",&j);
+	printf("Multiplication table for %d is:\n",j);
+	for(i=0; i<=10; i++)
+	{
+	  c=i*j;
+	  printf("%d*%d=%d \n",i,j,c);
+	}
 	
+    return 0;
 }
+
+#OUTPUT:
+Enter the number
+37
+Multiplication table for 37 is:
+0*37=0
+1*37=37
+2*37=74
+3*37=111
+4*37=148
+5*37=185
+6*37=222
+7*37=259
+8*37=296
+9*37=333
+10*37=370
