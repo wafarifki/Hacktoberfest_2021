62627925229981f6b0eb07e8105610b0460d13aa
Awal Ariansyah <57579406+snowfluke@users.noreply.github.com>
2021-10-03 18:48:31 +0700

    Create snowfluke.js

---
+++ Arithmetic/Javascript/snowfluke.js
@@ -0,0 +1,8 @@
+// Fisher-Yates shuffle
+const rng = arr => {
+    for (var i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+    }
+    return arr;
+}
