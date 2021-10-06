1696a582cd1bfeac192a70d4520f0311baaafcd0
Athallah Dzaki <discordgg5@gmail.com>
2021-10-06 07:24:06 +0700

    Create ChooseRandomFromArray.js

---
+++ Arithmatic/Javascript/ChooseRandomFromArray.js
@@ -0,0 +1,10 @@
+// You Can Use It For Free - AthallahDzaki
+
+function ChooseRandom(array)
+{
+    return array[Math.floor(Math.random * array.length)];
+}
+
+let array = ['Jhon', 'Dhoe', 'Elips', 'Ken', 'Kennedy', 'Josh', 'King', 'Lord', 'Queen'];
+
+console.log(ChooseRandom(array));
