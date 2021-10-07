18419dd29a0ed6e727a720df20a43bc1a2b75f25
Devesh Chandra <82612473+devesh1093@users.noreply.github.com>
2021-10-07 17:29:02 +0530

    Update Intersection_of_two_sets.c

--- Arithmetic/Intersection_of_two_sets.c
+++ Arithmetic/Intersection_of_two_sets.c
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 
