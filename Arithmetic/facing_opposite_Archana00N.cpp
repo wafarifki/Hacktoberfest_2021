2b0898988769278988ba360d27489ee5f0d220a3
Archana00N <b20082@students.iitmandi.ac.in>
2021-10-08 11:09:43 +0530

    Modified program desc

--- Arithmetic/facing_opposite_Archana00N.cpp
+++ Arithmetic/facing_opposite_Archana00N.cpp
@@ -1,6 +1,8 @@
-/*  If an even numbr of people stand in a circle and get numbered clockwise, everyone is facing someone else through the centre of the circle i.e. opposite to the prior. 
+/*  If an even numbr of people stand in a circle and get numbered clockwise, everyone is facing someone else
+through the centre of the circle i.e. opposite to the prior. 
 
-This program will output the 'number' of the person who is being looked at by 'c' if 'a' is looking at 'b'. But in some cases it is not possible. It will output "-1" in those cases.  */
+This program will output the 'number' of the person who is being looked at by 'c' if 'a' is looking at 'b'.
+But in some cases it is not possible. It will output "-1" in those cases.  */
 
 // Program by Archana Nayak
 #include <bits/stdc++.h>
