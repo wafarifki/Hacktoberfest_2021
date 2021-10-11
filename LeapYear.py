f319239421500a62b59316fede7dee7a27e2396d
Nawodya Jayalath <nawodyaonline@gmail.com>
2021-10-11 21:45:33 +0530

    leapYear checker

---
+++ LeapYear.py
@@ -0,0 +1,17 @@
+import os
+clear = lambda: os.system('clear')
+clear()
+
+year = int(input("Enter a year to check :   "))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
