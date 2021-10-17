890d25907baab7da1ab1b4d02005734a064a19b7
Saloni Shukla <48806039+salonishukla206@users.noreply.github.com>
2021-10-17 17:00:12 +0530

    Update sharpness.py

--- sharpness.py
+++ sharpness.py
@@ -10,6 +10,6 @@ import numpy as np
 #Using glob to read the image, Main reason of using Glob here because it help in reading multiple images at a time by using the syntax *.JPG..
 for img_name in glob.glob("C:/Users/saloni.shukla/Downloads/tesla.JPG"):
     img=cv2.imread(img_name)
-    iqa = DOM()
+    iqa = DOM() #DOM is a python packaged of opencv
     score = iqa.get_sharpness(img)
     print('sharpness image read')
