2b47c6811a3c3fd482dde928d92988b13135b9e5
Shivam Dubey <shivamdubey486@gmail.com>
2021-10-18 09:48:40 +0530

    added codechef solutions

---
+++ Codechef Solution/FLOW002.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int num1,num2,rem;
+		cin>>num1;
+		cin>>num2;
+		rem=num1%num2;
+	
+		cout<<rem<<endl;
+	}
+	return 0;
+}
