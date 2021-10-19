ae163674aac60b4ad60397e3c9f48f5519b3dc6b
rahmrny12 <rahmatprayogo12@gmail.com>
2021-10-19 18:34:10 +0700

    added program to order fibonacci

--- Arithmetic/php/rahmrny12.php
+++ Arithmetic/php/rahmrny12.php
@@ -15,16 +15,40 @@
 	echo hitung_umur("2022-07-12");
 
 	// ======================================================
-
+	
 	function hitung_pangkat($bilangan, $pangkat) {
 		if (!is_numeric($bilangan)) {
 			return "Bilangan harus bernilai angka";
 		} else if (!is_numeric($pangkat)) {
 			return "Pangkat harus bernilai angka";
 		}
-
+		
 		$hasil = pow($bilangan, $pangkat);
 		return "$bilangan pangkat $pangkat = " . $hasil;
 	}
-
+	
 	echo hitung_pangkat(2, 4);
+
+	// ======================================================
+
+	function print_deret_fibonacci($jumlah)
+	{
+		$angka_sebelumnya = 0;
+		$angka_sekarang = 1;
+		
+		$hasil = "$angka_sebelumnya $angka_sekarang";
+		
+		for ($i = 0; $i < $jumlah-2; $i++)
+		{
+			$angka_selanjutnya = $angka_sekarang + $angka_sebelumnya;
+			$hasil = $hasil . " $angka_selanjutnya";
+		
+			$angka_sebelumnya = $angka_sekarang;
+			$angka_sekarang = $angka_selanjutnya;
+		}
+
+		return $hasil;
+	  
+	}
+	  
+	echo print_deret_fibonacci(10);
