b99ecf0ae16ae90ef0ac93de8007e0752db6c6d6
harshilgupta-developer <92732673+harshilgupta-developer@users.noreply.github.com>
2021-10-19 21:37:56 +0530

    factorial

---
+++ factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    if(n==0){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
+int main(){
+    cout<<factorial(5)<<endl;
+
+    return 0;
+}
